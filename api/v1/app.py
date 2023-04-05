770efa17819ca005a7a41a804f332c2db4d120aa
dn004 <d.ntwari@alustudent.com>
2023-04-05 10:51:19 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+   Comment
+"""
+from flask import Flask, render_template, jsonify, make_response
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear_down(self):
+    """calls storage.close()"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """error handler for page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(
+        getenv("HBNB_API_PORT", "5000")), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+comment
+"""
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """
+    returns "status": "OK"
+    """
+    return jsonify({"status": "OK"})
+
